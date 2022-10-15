e8f2af849676ed3ac5fd1dcd4fae626f3130496e
knuxify <knuxify@gmail.com>
2022-10-15 18:17:40 +0200

    adapt to self.content move

--- __widget__.py
+++ __widget__.py
@@ -3,7 +3,7 @@
 Example widget template.
 """
 from aspinwall.widgets import Widget
-from gi.repository import Gtk, Adw
+from gi.repository import Gtk
 translatable = lambda message: message
 
 class MyWidget(Widget):
@@ -19,10 +19,14 @@ class MyWidget(Widget):
         super().__init__(instance)
         _ = self.l
 
+        self.content = Gtk.Box(hexpand=True, orientation=Gtk.Orientation.VERTICAL)
+
         example_label = Gtk.Label(
             label=_('Replace me with the widget content!')
         )
 
         self.content.append(example_label)
+
+        self.set_child(self.content)
 
 _widget_class = MyWidget
